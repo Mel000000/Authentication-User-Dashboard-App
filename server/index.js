c47239811a33cb5d195c14ed851dde3be7267180
Melina Reisinger <melina.reisinger07@gmail.com>
2026-05-22 15:12:13 +0300

    configuring cors

--- server/index.js
+++ server/index.js
@@ -29,8 +29,10 @@ const app = express()
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://authentication-user-dashboard-app.onrender.com', 
-    credentials: true 
+  origin: "https://authentication-user-dashboard-app.onrender.com", 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(cookieParser());
