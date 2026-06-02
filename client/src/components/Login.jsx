a2623a4c3a2f10f76c322e42836698b4b1410cf0
Melina Reisinger <melina.reisinger07@gmail.com>
2026-06-02 15:39:17 +0300

    fixing import

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';    
+import { useState, useEffect } from 'react';    
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
